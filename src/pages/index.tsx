cada04dd05ad8eb85dc1e3016a320f9252c7b97e
Sebastian Acosta Alonso <45018754+jaggervlad@users.noreply.github.com>
2023-09-14 12:00:10 -0500

    add page title and description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,25 @@ import { BooksList } from '@/components/books-list';
 import { Header } from '@/components/header';
 import { Sidebar } from '@/components/sidebar';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <Header />
-      <div className="flex h-full overflow-hidden bg-background lg:min-h-[calc(100vh-4rem)]">
-        <Sidebar />
-        <section className="container flex-1 h-full py-3 overflow-hidden overflow-y-auto lg:ml-64">
-          <BooksList />
-        </section>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>ReadHub</title>
+        <meta name="description" content="Aplicación de libros" />
+      </Head>
+
+      <main className="flex flex-col">
+        <Header />
+        <div className="flex h-full overflow-hidden bg-background lg:min-h-[calc(100vh-4rem)]">
+          <Sidebar />
+          <section className="container flex-1 h-full py-3 overflow-hidden overflow-y-auto lg:ml-64">
+            <BooksList />
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
